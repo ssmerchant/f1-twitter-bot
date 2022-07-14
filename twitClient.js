905029ef091657f18a8d573ced092dab54e74330
Samuel Scott Merchant <103539397+ssmerchant@users.noreply.github.com>
2022-07-14 15:12:31 -0500

    Update twitClient.js

--- twitClient.js
+++ twitClient.js
@@ -1,10 +1,10 @@
 const {TwitterApi} = require("twitter-api-v2")
 
 const client = new TwitterApi({
-    appKey: "2q2zAu1V7RmbyZ9dMpReN7klB",
-    appSecret: "36FqYmFDI3TDh33xLB9kth9vtqItEhHb6kJ3UFI1JrZ68Xzdzs",
-    accessToken: "1543763322780221441-UN5Qz8eJuvpJJ1UlEUL2MXtU9uAwWa",
-    accessSecret: "BOzXLPJbFqDI4aQD6Megst61vo8se9mKuQhebO2yftmkV"
+    appKey: "your appKey here",
+    appSecret: "your appSecret here",
+    accessToken: "your accessToken here",
+    accessSecret: "your accessSecret here"
 })
 
 const rwClient = client.readWrite
